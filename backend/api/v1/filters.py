51d3854c90be171f10dee957742b4d34fea9fbf5
Linar Aleev <aleevlr@yandex.ru>
2025-05-21 17:27:28 +0400

    updated recipe Filter

--- backend/api/v1/filters.py
+++ backend/api/v1/filters.py
@@ -1,4 +1,5 @@
 from django.db.models import Case, IntegerField, Q, Value, When
+from django.utils.translation import gettext_lazy as _
 from django_filters import rest_framework
 from recipes.models import Ingredient, Recipe, Tag
 
@@ -18,24 +19,34 @@ class RecipeFilter(rest_framework.FilterSet):
     ingredients = rest_framework.CharFilter(
         method='filter_ingredients_icontains')
     is_favorited = rest_framework.BooleanFilter(
-        method='filter_user_is_in_favorite')
+        method='filter_is_favorited',
+        label=_('В избранном'),
+        help_text=_('Показыать рецепты которые находятся в вашем списке '
+                    'избоанного'))
     is_in_shopping_cart = rest_framework.BooleanFilter(
-        method='filter_user_is_in_shopping_cart')
-
-    def filter_ingredients_icontains(self, queryset, name, value):
-        return queryset.filter(ingredients__name__icontains=value)
-
-    def filter_user_is_in_favorite(self, queryset, name, value):
-        return queryset.filter(favorites__user=self.request.user.id)
-
-    def filter_user_is_in_shopping_cart(self, queryset, name, value):
-        return queryset.filter(shopping_list__user=self.request.user.id)
+        method='filter_is_in_shopping_cart',
+        label=_('В списке покупок'),
+        help_text=_('Показыать рецепты которые находятся в вашем списке '
+                    'покупок'))
 
     class Meta:
         model = Recipe
         fields = (
             'is_favorited', 'is_in_shopping_cart', 'author', 'tags',
             'ingredients')
+
+    def filter_ingredients_icontains(self, queryset, name, value):
+        return queryset.filter(ingredients__name__icontains=value)
+
+    def filter_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated:
+            return queryset.filter(favorites__user=self.request.user.id)
+        return queryset.none()
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated:
+            return queryset.filter(shopping_list__user=self.request.user.id)
+        return queryset.none()
 
 
 class IngredientFilter(rest_framework.FilterSet):
