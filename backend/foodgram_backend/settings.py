246b9aa9f2140adf8fc729192e243af8a97ee8b4
Linar Aleev <aleevlr@yandex.ru>
2025-05-21 13:53:57 +0400

    added CSRF_TRUSTED_ORIGINS to settings and .env

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -14,7 +14,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'http://127.0.0.1:8000').split(' ')
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1:8000').split(' ')
 
 
 # Application definition
