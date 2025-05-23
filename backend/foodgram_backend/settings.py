7581bc550a1ee80f3518ce07689ffd6774b9212a
Linar Aleev <aleevlr@yandex.ru>
2025-05-24 01:17:43 +0400

    bug fixed

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -123,8 +123,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.BaseLimitOffsetPagination',
+    )
 }
 
 
